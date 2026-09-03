fc9a131769db85e189332cf80ec2e19c8da4463e
Russell-Frost <rio.yrgubay@gmail.com>
2026-09-03 23:11:42 +0800

    Activity 2

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 import express from 'express';
 
-const students = [{id: 0, name: "student1"}]
+const students = [ 
+  { id: 1,
+    name: "Russell",
+    course: "Computer Science"
+  },
+  { 
+    id: 2,
+    name: "Lilian",
+    course: "Computer Science",
+  },
+  {  
+    id: 3,
+    name: "Jackilyn",
+    course: "Computer Science"
+  },
+  {  
+    id: 4,
+    name: "Isabella",
+    course: "Information Technology"
+  },
+  {
+    id: 5,
+    name: "Kenneth",
+    course: "Information Technology"
+  }
+]
 
 const app = express()
 
@@ -10,23 +35,33 @@ app.get("/students", (req, res) => {
     res.send(students);
 });
 
-app.post("/students", (req, res) => {
-    const newStudent = req.body
-
-    students = [...students, newStudent];
-
-    res.send(newStudent);
+app.get('/student/:id', (req, res) => {
+  const student = students.find(s => s.id === parseInt(req.params.id));
+  if (!student) return res.status(404).send('Student not found');
+  res.send(student);
 });
 
-app.patch("/students/:index", (req, res) => {
-    const studentIndex = req.params
-    const updatedStudentData = req.body
+app.post('/student', (req, res) => {
+  const newStudent = {
+    id: students.length ? students[students.length - 1].id + 1 : 1,
+    name: req.body.name,
+  };
+  students.push(newStudent);
+  res.send(newStudent);
+});
 
-    students[studentIndex] = updatedStudentData;
+app.patch('/student/:id', (req, res) => {
+  const student = students.find(s => s.id === parseInt(req.params.id));
+  if (!student) return res.status(404).send({message: "Student not found"});
+  Object.assign(student, req.body);
+  res.send(student);
+});
 
-    res.send(students[studentIndex]);
-
-    //to be impemented
+app.delete('/student/:id', (req, res) => {
+  const studentIndex = students.findIndex(s => s.id === parseInt(req.params.id));
+  if (studentIndex === -1) return res.status(404).send({message: "Student not found"});
+  const deletedStudent = students.splice(studentIndex, 1);
+  res.send(deletedStudent[0]);
 });
 
 app.listen(3000, () => {
